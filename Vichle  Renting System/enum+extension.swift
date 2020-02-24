b77d290647e068fe0c4ff86ed20e3a135cc2db38
manbeer gill <manbeergill545@gmail.com>
2020-02-24 18:36:14 -0500

    (c0768706)adding dataset function

--- Vichle  Renting System/enum+extension.swift
+++ Vichle  Renting System/enum+extension.swift
@@ -20,78 +20,14 @@ extension String{
         }
     
     func dateSet(input:String)->String
-    {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MM/dd/yyyy"
-    if let date = formatter.date(from: input) {
-        formatter.locale = Locale(identifier: "en_US")
-        formatter.dateStyle = .long
-        return(formatter.string(from: date))
-        }
-        return "Incorrect Date"
-    }
-    func encrypt(password:String)->String
-    {
-     let charArray=Array(password)
-        var s1=String()
-        for i in charArray
-        {
-            var a=i.asciiValue
-            a=a!+1
-            s1 = s1+String(UnicodeScalar(UInt8?(a!)!))
-            }
-        
-        return s1
-        
-    }
-    func decrypt(password:String)->String{
-        let charArray=Array(password)
-        var s1=String()
-        for i in charArray
-        {
-            var a=i.asciiValue
-            a=a!-1
-        s1 = s1+String(UnicodeScalar(UInt8?(a!)!))
-        }
-       
-        return s1
-        
-    }
-   
-    }
-extension Int{
-    
-   func isMobileNumberValid(mobile:Int) -> Bool{
-    return true/*//https://stackoverflow.com/questions/27998409/email-phone-validation-in-swift
-    let mobileNumberRegEx = "[0-9]{10}"
-    let mobileTest = NSPredicate(format: "SELF MATCHES %@", mobileNumberRegEx)
-    return mobileTest.evaluate(with: mobile)*/
-    }
-    func km()->String{
-        return "\(self)km"
-    }
-    func currency() -> String
-           {
-               return "$\(self)"
-           }
-    func speed()->String
-    {
-        return "\(self)km\\hr"
-    }
-}
-
-     extension Double{
-        func currency() -> String
-        {
-            return "$\(self)"
-        }
-    }
-
-extension Float{
-       func currency() -> String
        {
-           return "$\(self)"
+       let formatter = DateFormatter()
+       formatter.dateFormat = "MM/dd/yyyy"
+       if let date = formatter.date(from: input) {
+           formatter.locale = Locale(identifier: "en_US")
+           formatter.dateStyle = .long
+           return(formatter.string(from: date))
+           }
+           return "Incorrect Date"
        }
-   }
-
-
+      
