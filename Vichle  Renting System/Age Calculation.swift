02d7946e0201f3da853bf6125107432a8372ac97
manbeer gill <manbeergill545@gmail.com>
2020-02-24 18:32:03 -0500

    adding function(c0768706)

--- Vichle  Renting System/Age Calculation.swift
+++ Vichle  Renting System/Age Calculation.swift
@@ -9,13 +9,13 @@
 import Foundation
 public class AgeCalculation{
     
-   static func stringToDate(string : String) -> Date {      //https://stackoverflow.com/questions/35700281/date-format-in-swift
-        
-        let dateformatGet1 = DateFormatter()
-        dateformatGet1.dateFormat = "dd/MM/yyyy"
-     
-        let birthDate1 : Date? = dateformatGet1.date(from: string)
-        return birthDate1!
+  static func stringToDate(string : String) -> Date {      //https://stackoverflow.com/questions/35700281/date-format-in-swift
+         
+         let dateformatGet1 = DateFormatter()
+         dateformatGet1.dateFormat = "dd/MM/yyyy"
+      
+         let birthDate1 : Date? = dateformatGet1.date(from: string)
+         return birthDate1!
     }
     
 static func calculateAge(birthDate:String)->String//https://stackoverflow.com/questions/33780184/calculate-duration-between-date-ios-in-years-months-and-date-format
